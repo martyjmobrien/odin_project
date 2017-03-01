2b95c484c955b065e334511020d49bf7ab8304e7
Marty O'Brien <marty.jm.obrien@gmail.com>
2017-03-01 15:03:54 +0000

    Etch-A-Sketch game update

--- etch-a-sketch/script/etch-a-sketch.js
+++ etch-a-sketch/script/etch-a-sketch.js
@@ -1,37 +1,33 @@
-var rows = 16
-var squares = 16
-
 $(document).ready(function() {
+    createBoard(20)
+});
 
-    var totalSquares = rows * squares;
+function createBoard(newSquares) {
+    var squareSize = 600 / newSquares + ".px";
+
+    var totalSquares = newSquares * newSquares;
     
     for ( i = 0; i < totalSquares; i++) {
         $('.container').append('<div class="square">');
     };
 
+    $('.square').css({ "width": squareSize, "height": squareSize })
+
+    hoverDraw();
+}
+
+function hoverDraw() {
     $('.square').hover(function () {
         var opacityAmount = 0.1
         if (opacityAmount < 1) {
             $(this).css({ "background-color": "#000000", "opacity": opacityAmount })
         };
         opacityAmount += 0.1
-        console.log(opacityAmount)
     });
-
-});
+}
 
 function resetBoard() {
-    
     $('.container').empty();
-    
-    var totalSquares = rows * squares;
-    
-    for ( i = 0; i < totalSquares; i++) {
-        $('.container').append('<div class="square">');
-    };
-
-    $('.square').hover(function () {
-        $(this).css("background-color", "black");
-    });
-
+    var userSquares = parseInt(prompt("How many squares would you like? Numbers only please"), 10);
+    createBoard(userSquares);
 };

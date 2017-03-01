7ee9759d4e8cfa4e29d3b65ff600bdb46a2d4501
Marty O'Brien <marty.jm.obrien@gmail.com>
2017-03-01 22:43:14 +0000

    addition of color selector, eraser and click/drag movement as instead of hover

--- etch-a-sketch/script/etch-a-sketch.js
+++ etch-a-sketch/script/etch-a-sketch.js
@@ -1,9 +1,28 @@
 $(document).ready(function() {
+    var squareColor = "#000000"
     createBoard(20)
+    $('#black').click(function() {
+        hoverDraw("#000000");
+    });
+    $('#red').click(function() {
+        hoverDraw("#ff0000");
+    });
+    $('#blue').click(function() {
+        hoverDraw("#0000ff");
+    });
+    $('#green').click(function() {
+        hoverDraw("#008000");
+    });
+    $('#yellow').click(function() {
+       hoverDraw("#ffff00");
+    });
+    $('#eraser').click(function() {
+        hoverDraw("#ffffff");
+    });
 });
 
 function createBoard(newSquares) {
-    var squareSize = 600 / newSquares + ".px";
+    var squareSize = Math.ceil(600 / newSquares) + ".px";
 
     var totalSquares = newSquares * newSquares;
     
@@ -13,27 +32,21 @@ function createBoard(newSquares) {
 
     $('.square').css({ "width": squareSize, "height": squareSize })
 
-    hoverDraw();
+    hoverDraw("#000000");
 }
 
-function hoverDraw() {
-    // var opacityAmount = 0.1
-    $('.square').hover(
-        function() {
-            // if (opacityAmount < 1.0) {
-                // $(this).css({ "background-color": "#000000", "opacity": opacityAmount });
-                $(this).css({ "background-color": "#000000" });
-                // opacityAmount += 0.1
-            // };
-        }, function() {
-            // $(this).css({ "background-color": "#000000", "opacity": opacityAmount });
-            $(this).css({ "background-color": "#000000" });
-        }
-    );
+function hoverDraw(squareColor) {
+    $('.square').mousedown(function() {
+        $('.square').mousemove(function(e) {
+            if(e.which==1) {
+                $(this).css({ "background-color": squareColor });
+            }
+        });
+    });
 }
 
 function resetBoard() {
     $('.container').empty();
-    var userSquares = parseInt(prompt("How many squares would you like? Numbers only please"), 10);
+    var userSquares = parseInt(prompt("Which pixel size would you like?? Numbers only please"));
     createBoard(userSquares);
 }

a1ba7f5038cc9099e9992e867717495f57b53da8
Marty O'Brien <marty.jm.obrien@gmail.com>
2017-03-07 21:30:34 +0000

    Modal exploration

---
+++ pixel-a-sketch/script/modal.js
@@ -0,0 +1,8 @@
+var newBoardButton = document.getElementById("newBoardModal");
+
+$(document).ready(function() {
+    $('.startModal').css({ display: "block" });
+    $("newBoardButton").onclick(function() {
+        $('.newBoardModal').css({ display: "block" });
+});
+
